e8e4ffde88a26599af55b3b13c7e94010ddda68b
Shafley <shafleyxia@gmail.com>
2017-05-25 14:12:25 +0800

    fix format

--- src/index.js
+++ src/index.js
@@ -34,7 +34,8 @@
         return ret
     }
 
-    function format(date, fmt) {
+    function format(val, fmt) {
+        var date = new Date(val)
         var o = {
             "M+": date.getMonth() + 1,
             "d+": date.getDate(),
@@ -46,10 +47,12 @@
         }
         if (/(y+)/.test(fmt))
             fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length))
-        for (var k in o)
-            if (new RegExp("(" + k + ")").test(fmt))
+        for (var k in o){
+            if (new RegExp("(" + k + ")").test(fmt)){
                 fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k])
                     .length)))
+            }
+        }
         return fmt
     }
 
